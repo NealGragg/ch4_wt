b458591adbc8aa994060b1c25e4b5a7cd3aab71c
nealgragg <ngragg@uncc.edu>
2016-02-26 22:31:36 +0000

    Done with section 4.5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   end
 
 def show
-  @movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
+  @movie = Movie.find_by_id(params[:id]) # what if this movie  not in DB?
   # BUG: we should check @movie for validity here!
 end
 #add below all otehr methods
