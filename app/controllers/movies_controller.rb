d7ae728cc97f11e9f916a6bffb4d646a94fce945
nealgragg <ngragg@uncc.edu>
2016-02-26 22:40:32 +0000

    Done with section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,11 @@ def show
   @movie = Movie.find_by_id(params[:id]) # what if this movie  not in DB?
   # BUG: we should check @movie for validity here!
 end
+
+def new
+  # default: render 'new' template
+end
+
 #add below all otehr methods
 private
 
