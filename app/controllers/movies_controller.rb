0649249bf9cd3ae6333fb6e4ff0c1a91b6385e9c
nealgragg <ngragg@uncc.edu>
2016-02-27 14:40:17 +0000

    Done with section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,12 @@ def new
   # default: render 'new' template
 end
 
+def create
+  @movie = Movie.create!(movie_params)
+  flash[:notice] = "#{@movie.title} was successfully created."
+  redirect_to movies_path
+end
+
 #add below all otehr methods
 private
 
